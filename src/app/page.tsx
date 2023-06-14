efb4255c36be912536a085f6629091118c007cc4
Eunji <codediverj@gmail.com>
2023-06-14 09:23:30 -0700

    added layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,12 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <header>My trip</header>
+      <div>Continue Planning</div>
+      <div>Completed</div>
+      <div>I plan trip with...</div>
+    </main>
+  );
 }
