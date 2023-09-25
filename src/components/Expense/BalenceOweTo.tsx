f5ec42f59b6dbdf1aecc364f23cca5a40c15c37f
Eunji <codediverj@gmail.com>
2023-09-24 17:56:18 -0700

    added showing owe to someone

--- src/components/Expense/BalenceOweTo.tsx
+++ src/components/Expense/BalenceOweTo.tsx
@@ -1,5 +1,4 @@
-import { useEffect } from "react";
-import Image from "next/image";
+import { usePlanDataStore } from "@/contexts/planData/planData.provider";
 import { useExpenseDataStore } from "@/contexts/expenseData/expenseData.provider";
 import { useUserDataStore } from "@/contexts/userData/userData.provider";
 import ExpenseItem from "./ExpenseItem";
@@ -7,40 +6,86 @@ import ExpenseItem from "./ExpenseItem";
 function BalenceOweTo() {
   const userData = useUserDataStore();
   const allExpenseData = useExpenseDataStore();
+  const planContextData = usePlanDataStore();
 
-  //
+  const groupAndPaidExpenses = allExpenseData.filter((data) => {
+    return (
+      data.isGroupActivity &&
+      data.Single_Plan_Expense.length > 0 &&
+      data.Single_Plan_Expense.every((expenseItem) => expenseItem.paidUser !== undefined)
+    );
+  });
+
+  function makeList(selectedJoinUser: string, owe: number) {
+    const filteredExpenses = groupAndPaidExpenses.filter((data) => {
+      return data.Single_Plan_Expense.some((expenseItem) => {
+        if (owe === 1) {
+          return (
+            expenseItem.paidUser?.paidUserId === selectedJoinUser &&
+            expenseItem.attendedUser.attendedUserId === userData.userId
+          );
+        }
+        if (owe === 2) {
+          return (
+            expenseItem.paidUser?.paidUserId === userData.userId &&
+            expenseItem.attendedUser.attendedUserId === selectedJoinUser
+          );
+        }
+      });
+    });
+    return filteredExpenses.map((data) => <ExpenseItem key={data.singlePlanId} data={data} />);
+  }
 
   return (
-    <div className="owe_expense">
-      <h4 className="owe_expense_title">
-        <Image
-          src="/expense_arrow_right.svg"
-          alt="friend name"
-          width="24"
-          height="24"
-          className="profile image"
-        />
-        <span>{`'s Balence owe to`}</span>
-        <Image
-          src="/expense_arrow_right.svg"
-          alt="friend name"
-          width="24"
-          height="24"
-          className="profile image"
-        />
-      </h4>
-      <div className="owe_expense_con">
-        <ul className="expense_list">
-          {allExpenseData.map((data) => (
-            <ExpenseItem key={data.singlePlanId} data={data} />
-          ))}
-        </ul>
-        <div className="send_money">
-          <p>Did you repay the money owed to {`'A'`} ?</p>
-          <button>Yes, I did</button>
-        </div>
-      </div>
-    </div>
+    <>
+      {/* 1. 타인이 Paid */}
+      {planContextData.peopleJoin
+        .filter((joinUser) => joinUser.userId !== userData.userId)
+        .map((joinUser) => {
+          const expenseList = makeList(joinUser.userId, 1);
+          if (expenseList.length === 0) {
+            return null;
+          }
+          return (
+            <div className="owe_expense" key={joinUser.userId}>
+              <h4 className="owe_expense_title">
+                <span className="me">My</span>
+                <span className="owe_span1">{`Balence owe to `}</span>
+                {joinUser.nickname}
+              </h4>
+              <div className="owe_expense_con purple">
+                <ul className="expense_list">{expenseList}</ul>
+                <div className="send_money">
+                  <p>Did you repay the money owed to {joinUser.nickname} ?</p>
+                  <button>Yes, I did</button>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+
+      {/* 2. 내가 Paid */}
+      {planContextData.peopleJoin
+        .filter((joinUser) => joinUser.userId !== userData.userId)
+        .map((joinUser) => {
+          const expenseList = makeList(joinUser.userId, 2);
+          if (expenseList.length === 0) {
+            return null;
+          }
+          return (
+            <div className="owe_expense" key={joinUser.userId}>
+              <h4 className="owe_expense_title">
+                {joinUser.nickname}
+                <span className="owe_span2">{`'s Balence owe to `}</span>
+                <span className="me">Me</span>
+              </h4>
+              <div className="owe_expense_con">
+                <ul className="expense_list">{makeList(joinUser.userId, 2)}</ul>
+              </div>
+            </div>
+          );
+        })}
+    </>
   );
 }
 
--- src/utils/myExpense.utils.ts
+++ src/utils/myExpense.utils.ts
@@ -17,5 +17,6 @@ export function myExpense(expenseData: ExpenseItemType[], userId: string) {
   const expenseItem = expenseData.find(
     (expenseItem) => expenseItem.paidUser && expenseItem.paidUser.paidUserId === userId
   );
-  return expenseItem ? expenseItem.expense : 0;
+  const expenseOthers = expenseData[0].expense;
+  return expenseItem ? expenseItem.expense : expenseOthers;
 }
