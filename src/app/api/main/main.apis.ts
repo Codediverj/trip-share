327b1b374fd7456f4ec851862237e46ec8b618d6
Eunji <codediverj@gmail.com>
2023-10-07 21:48:31 -0700

    added plan with... on main page

---
+++ src/app/api/main/main.apis.ts
@@ -0,0 +1,36 @@
+import { SupabaseClient } from "@supabase/supabase-js";
+import { Database } from "@/supabase.types";
+
+export const getMyPlanIds = async (supabase: SupabaseClient<Database>, userId: string) => {
+  const { data, error } = await supabase
+    .from("People_Join")
+    .select("plan_id")
+    .eq("user_id", userId);
+
+  if (error) {
+    console.error("Error fetching plan_ids:", error.message);
+    return [];
+  }
+  const planIds = data.map((row) => row.plan_id);
+  return planIds;
+};
+
+export const getOtherUserIds = async (
+  supabase: SupabaseClient<Database>,
+  planIds: string[],
+  userId: string
+) => {
+  const { data, error } = await supabase
+    .from("People_Join")
+    .select("user_id, User(*)")
+    .in("plan_id", planIds);
+
+  if (error) {
+    console.error("Error fetching other user_ids:", error.message);
+    return [];
+  }
+
+  const otherUserIds = data.filter((row) => row.user_id !== userId);
+
+  return otherUserIds;
+};
--- src/components/HomePage/PlanWithList.tsx
+++ src/components/HomePage/PlanWithList.tsx
@@ -1,32 +1,57 @@
+import { useEffect, useState } from "react";
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./HomePage.module.scss";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { getMyPlanIds, getOtherUserIds } from "@/app/api/main/main.apis";
+
+// context
+import { useUserDataStore } from "@/contexts/userData/userData.provider";
+import { User } from "@/app/api/user/user.types";
+
+const supabase = createClientComponentClient();
 
 function PlanWithList({ randomImage }: any) {
+  const userData = useUserDataStore();
+  const [otherUserData, setOtherUserData] = useState([{}]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (userData.userId) {
+          const myPlanIds = await getMyPlanIds(supabase, userData.userId);
+          const otherUserIds = await getOtherUserIds(supabase, myPlanIds, userData.userId);
+
+          setOtherUserData(otherUserIds);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, [supabase, userData.userId]);
+
+  console.log(otherUserData);
+
   return (
     <>
       <div className={styles.plan_with}>
         <h3 className={styles.dashboard_title}>I plan trip with...</h3>
+        {/* {otherUserData.length > 0 &&
+          otherUserData.map((friend) => (
+            <li key={friend.user_id}>
+              <Image
+                src={friend.User.profile_image ? friend.User.profile_image : "기본 이미지 URL"}
+                alt="profile image"
+                width="50"
+                height="50"
+              />
+              {friend.user_id}
+            </li>
+          ))} */}
+
         <ul>
-          <li>
-            <Image src={randomImage} alt="plus icon" width="50" height="50" />
-          </li>
-          <li>
-            <Image src={randomImage} alt="plus icon" width="50" height="50" />
-          </li>
-          <li>
-            <Image src={randomImage} alt="plus icon" width="50" height="50" />
-          </li>
-          <li>
-            <Image src={randomImage} alt="plus icon" width="50" height="50" />
-          </li>
-          <li>
-            <Image src={randomImage} alt="plus icon" width="50" height="50" />
-          </li>
-          <li>
-            <Image src={randomImage} alt="plus icon" width="50" height="50" />
-          </li>
           <li>
             <Image src={randomImage} alt="plus icon" width="50" height="50" />
           </li>
